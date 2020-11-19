3a2b87f5a4fd1d2cf3dfe0d013b7ab18f297a8f4
stanleympoyi <mpoyis@oregonstate.edu>
2020-11-19 14:29:50 -0700

    working on code

--- frontend/server.js
+++ frontend/server.js
@@ -3,8 +3,10 @@ const path = require("path");
 
 const app = express();
 
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
 app.listen(process.env.POST || 1430, () => console.log("Server running...."));
