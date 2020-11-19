6013ef78f9846db7c69d5a042c3452547763c2f3
stanleympoyi <mpoyis@oregonstate.edu>
2020-11-19 15:22:04 -0700

    dom listener

--- frontend/server.js
+++ frontend/server.js
@@ -9,4 +9,4 @@ app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
-app.listen(process.env.POST || 1430, () => console.log("Server running...."));
+app.listen(process.env.POST || 1430, () => console.log("Server running to Command - C to exit"));
